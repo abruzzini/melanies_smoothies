82b43eb990b17e0ed8ea846cb9eb72ecca093ba7
Santi Abruzzini <abruzzini.s@gmail.com>
2024-09-29 14:12:21 -0300

    cambios para lesson 10

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,12 @@ session = cnx.session()
 name_on_order = st.text_input('Name on Smoothie')
 st.write("The name on your Smoothie will be:", name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
+
+pd_df=my_dataframe.to_pandas()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients.'
@@ -34,6 +38,10 @@ if ingredients_list: #significa if ingredient list is not null
     #for each fruit_chosen in
     #ingredients_list multiselect box: do everything below this line that is indented
             ingredients_string += fruit_chosen + ' '#The += operator means "add this to what is already in the variable"
+
+            search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+            st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
             st.subheader(fruit_chosen + ' Nutrition Information')
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
             fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
