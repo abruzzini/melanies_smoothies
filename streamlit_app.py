3a99636a24f3ef3167c249c11376d4bcb80d22bd
Santi Abruzzini <abruzzini.s@gmail.com>
2024-09-29 13:36:23 -0300

    changes for lesson10

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,6 +52,10 @@ if ingredients_list: #significa if ingredient list is not null
         st.success("Your Smoothie is ordered " + name_on_order + "!", icon="✅")
 
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
 
 
